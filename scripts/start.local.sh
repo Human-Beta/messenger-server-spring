22f3188a32c63c9834fffe77907f115bbc595b69
Nikita Shishov <workmailnikita@gmail.com>
2022-07-10 20:18:42 +0300

    server: init

---
+++ scripts/start.local.sh
@@ -0,0 +1,4 @@
+export JAVA_HOME="C:\Program Files\Java\jdk-11.0.10"
+export PATH="C:\Program Files\Java\jdk-11.0.10\bin":$PATH
+
+java -jar build/libs/server-0.0.1-SNAPSHOT.jar
---
+++ src/test/java/com/nikita/messenger/server/ServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nikita.messenger.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
