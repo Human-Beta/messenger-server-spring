589a8cbe6052568bcf18634677e92f5ff8638503
Nikita Shishov <workmailnikita@gmail.com>
2022-08-17 21:44:52 +0300

    security: refactor config to not use deprecated classes

--- src/main/java/com/nikita/messenger/server/config/security/OAuthConfig.java
+++ src/main/java/com/nikita/messenger/server/config/security/OAuthConfig.java
@@ -5,25 +5,24 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+//TODO: rewrite to use last versions of libraries
 @Configuration
 @EnableAuthorizationServer
-public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
-    @Autowired
-    private AuthenticationManager authenticationManager;
-    @Autowired
-    private PasswordEncoder passwordEncoder;
-    @Autowired
-    private UserDetailsService userService;
-
+public class OAuthConfig implements AuthorizationServerConfigurer  {
     @Value("${jwt.clientId}")
     private String clientId;
     @Value("${jwt.client-secret}")
@@ -34,9 +33,11 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
     private int refreshTokenValiditySeconds;
     @Value("${jwt.authorizedGrantTypes}")
     private String[] authorizedGrantTypes;
-
     @Value("${jwt.scopes}")
     private String[] scopes;
+
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
     @Override
     public void configure(final AuthorizationServerSecurityConfigurer security) {
@@ -57,15 +58,47 @@ public class OAuthConfiguration extends AuthorizationServerConfigurerAdapter {
     }
 
     @Override
-    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
         endpoints
                 .accessTokenConverter(accessTokenConverter())
-                .userDetailsService(userService)
-                .authenticationManager(authenticationManager);
+                .userDetailsService(userDetailsService())
+                .authenticationManager(authenticationManager(authenticationConfiguration));
     }
 
     @Bean
     JwtAccessTokenConverter accessTokenConverter() {
         return new JwtAccessTokenConverter();
+    }
+
+    @Bean
+//    TODO: get from DB (JdbcUserDetailsManager? or just custom implementation of UserDetailsManager)
+    public InMemoryUserDetailsManager userDetailsService() {
+        final UserDetails user = User.builder()
+                .passwordEncoder(passwordEncoder()::encode)
+                .username("test_user")
+                .password("test_password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        final DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+
+        return provider;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(final AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
     }
 }
--- src/main/java/com/nikita/messenger/server/config/security/ServerSecurityConfig.java
+++ src/main/java/com/nikita/messenger/server/config/security/ServerSecurityConfig.java
@@ -1,71 +1,29 @@
 package com.nikita.messenger.server.config.security;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.core.Ordered;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true, proxyTargetClass = true)
-public class ServerSecurityConfig extends WebSecurityConfigurerAdapter {
+@AutoConfiguration
+@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+public class ServerSecurityConfig {
 
-    @Override
-    protected void configure(final HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(final HttpSecurity http) throws Exception {
         http
                 .sessionManagement().sessionCreationPolicy(STATELESS)
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
                 .anyRequest().authenticated();
+
+        return http.build();
     }
-
-    @Bean
-//    TODO: get from DB (JdbcUserDetailsManager? or just custom implementation of UserDetailsManager)
-    public InMemoryUserDetailsManager userDetailsService() {
-        final UserDetails user = User.builder()
-                .passwordEncoder(passwordEncoder()::encode)
-                .username("test_user")
-                .password("test_password")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager(user);
-    }
-
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        final DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-
-        provider.setPasswordEncoder(passwordEncoder());
-        provider.setUserDetailsService(userDetailsService());
-
-        return provider;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(10);
-    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
-//TODO: do i need it?
-//    @Bean
-//    public AuthenticationManager authenticationManager(
-//            final AuthenticationConfiguration authenticationConfiguration) throws Exception {
-//        return authenticationConfiguration.getAuthenticationManager();
-//    }
 }
